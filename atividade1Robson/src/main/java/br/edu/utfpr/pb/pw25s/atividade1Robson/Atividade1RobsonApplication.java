94204b7da92246337747ebc65c3e346102e275c9
Aluno <therobsonmuller@gmail.com>
2020-03-12 22:21:19 -0300

    a

---
+++ atividade1Robson/src/main/java/br/edu/utfpr/pb/pw25s/atividade1Robson/Atividade1RobsonApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.pb.pw25s.atividade1Robson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade1RobsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade1RobsonApplication.class, args);
+	}
+
+}
